652617021f56442556da2301e983df7d62e3111e
Tomilola <tomilolapromise@gmail.com>
2022-09-14 14:30:53 -0700

    wo

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-* main - prints _putchar
+* main - prints Putchar
 *
 * Description: prints _putchar
 *
@@ -8,12 +8,14 @@
 */
 int main(void)
 {
-char text[9] = "_putchar";
-int i = 0;
-for (i = 0; i < 8; i++)
-{
-_putchar(text[i]);
-}
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
 _putchar('\n');
 return (0);
 }
