731fe1ac2738d55f390a2e9a7c87e038a361d193
Tomilola <tomilolapromise@gmail.com>
2022-09-16 14:02:50 -0700

    God abeg

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,27 +1,29 @@
-#include "main.h"
+#include <stdlib.h>
+#include<stdio.h>
 /**
-* print_triangle - print a triangle, followed by new line
-* @size: an input integer
+* main - entry point
+*
 * Return: 0
 */
-void print_triangle(int size)
+int main(void)
 {
-int i = 0, j, n = size - 1;
-if (size > 0)
+long int i;
+long int n;
+long int d;
+n = 612852475143;
+for (i = 1; i <= n; i++)
 {
-for (; i < size; i++)
+if (n % i == 0)
 {
-for (j = 0; j < size; j++)
-{		
-if (j < n)
-_putchar(' ');
-else
-_putchar('#');
+if (n == i)
+{
+printf("%ld\n", i);
+break;
 }
-n--;
-_putchar('\n');
+d = n / i;
+n = d;
+i = 1;
 }
 }
-else
-_putchar('\n');
+return (0);
 }
