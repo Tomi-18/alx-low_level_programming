8f4b49a4bb1fbf31a6dde4e0e73c2c1511a681ef
Tomilola <tomilolapromise@gmail.com>
2022-09-19 15:28:51 -0700

    omooooo

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
-* _puts - print to the stdout
-* @str: string
-*
+* print_rev - prints a reverse string
+* @s: input string
 * Return: void
 */
-void _puts(char *str)
+void print_rev(char *s)
 {
 int index;
-for (index = 0; str[index] != '\0'; index++)
+for (index = 0; s[index] != '\0'; index++)
+;
+for (index = index - 1; s[index] != '\0'; index--)
 {
-_putchar(str[index]);
+_putchar(s[index]);
 }
 _putchar('\n');
 }
